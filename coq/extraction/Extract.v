27df1c607374ccc839ca6041e90652263cd9a7cb
Ryan Doenges <me@ryandoeng.es>
2021-08-16 16:38:43 -0400

    set up extraction and ocaml code

---
+++ coq/extraction/Extract.v
@@ -0,0 +1,5 @@
+Require Coq.extraction.Extraction.
+
+Require MiniP4.Syntax.
+
+Separate Extraction MiniP4.Syntax.
---
+++ lib/printer.ml
@@ -0,0 +1,7 @@
+open Coq_minip4.Syntax
+
+let print (e: expr) : unit =
+  match e with
+  | NumLit _ -> failwith "unimplemented"
+  | BinOp (_, _, _) -> failwith "unimplemented"
+
