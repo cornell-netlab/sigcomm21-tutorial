3107af6ab7dfbcfe0e29e3566500ca46f8ac413a
Nate Foster <jnfoster@cs.cornell.edu>
2021-08-23 16:12:47 -0700

    Run a packet through

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,29 @@
+open Coq_minip4
+open Minip4
+open Syntax
+open Interp
+
+
+let init_state = 
+  { store = [];
+    type_env = [];
+    pkt = [];
+    acts = [];
+    tables = []; }
+
+let defns = []
+
+let cmd = Block []
+
+let prog = defns,cmd
+
+let fuel = 100
+
 let () = 
-   Printf.eprintf "[Welcome to MiniP4]\n%!"
+   Format.printf "*** Welcome to MiniP4 ***\n%!";
+   Format.printf "Initial Packet: %a\n%!" Pp.to_fmt (Printer.format_bitstring init_state.pkt);
+   match interp_prog fuel init_state prog with 
+   | None -> 
+     Format.printf "[Error]"
+   | Some state -> 
+     Format.printf "Final Packet: %a\n%!" Pp.to_fmt (Printer.format_bitstring state.pkt)
