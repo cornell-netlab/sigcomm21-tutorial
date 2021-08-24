6a261eb8e76d6931513098bcbd1fb8f4c80e5bf7
Nate Foster <jnfoster@cs.cornell.edu>
2021-08-23 17:26:42 -0700

    Replace Pervasives.succ with Stdlib.succ

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all:
 	touch coq/extraction/Extract.v
 	make -C coq
 	dune build
+
+clean:
+	make -C coq clean
--- coq/extraction/Extract.v
+++ coq/extraction/Extract.v
@@ -1,6 +1,11 @@
 Require Coq.extraction.Extraction.
 Require Coq.extraction.ExtrOcamlBasic.
+
+
 Require Coq.extraction.ExtrOcamlNatInt.
+Extract Inductive nat => int [ "0" "Stdlib.succ" ]
+ "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
+
 Require Coq.extraction.ExtrOcamlNativeString.
 
 Require MiniP4.Syntax.
