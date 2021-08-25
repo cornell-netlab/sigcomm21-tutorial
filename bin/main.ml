336ee0f40bc198ac1830cf1e19e6282f8bf8c44e
Ryan Doenges <me@ryandoeng.es>
2021-08-25 16:16:10 -0400

    fix main

--- bin/main.ml
+++ bin/main.ml
@@ -3,26 +3,27 @@ open Minip4
 open Syntax
 open Interp
 
-let init_state = 
-  { store = [];
-    type_env = [];
-    pkt = [];
-    acts = [];
+let init_def_state = 
+  { type_env = [];
     tables = [];
     rules = []; }
 
+let init_state = 
+  { store = [];
+    pkt = [] }
+
 let defns = []
 
-let cmd = Block []
+let cmd = Nop
 
-let prog = defns,cmd
+let prog = defns, cmd
 
 let fuel = 100
 
 let () = 
    Format.printf "*** Welcome to MiniP4 ***\n%!";
    Format.printf "Initial Packet: %a\n%!" Pp.to_fmt (Printer.format_bitstring init_state.pkt);
-   match interp_prog fuel init_state prog with 
+   match interp_prog fuel init_def_state init_state prog with 
    | None -> 
      Format.printf "[Error]"
    | Some state -> 
